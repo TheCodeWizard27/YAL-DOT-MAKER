d212c241bc22f69558b1b00ad5c71fd7ad8fcd6c
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-08 10:34:26 +0100

    Added Game Class with some attributes

--- yal-dot-player/src/core/Game.java
+++ yal-dot-player/src/core/Game.java
@@ -1,5 +1,15 @@
 package core;
 
-public class Game {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Game {
+	private boolean run = true;
+	private Set<Integer> keyBuffer = new HashSet<Integer>();
+	private DrawEngine drawengine;
+	
+	public Game() {
+		this.drawengine = new DrawEngine(this);
+	}
+	
 }
