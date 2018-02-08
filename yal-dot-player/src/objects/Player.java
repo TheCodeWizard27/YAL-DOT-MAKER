b876fb8d510bc106c81347a7ecae084e46ffdc39
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-08 14:11:17 +0100

    Added Sprite getter setter

--- yal-dot-player/src/objects/Player.java
+++ yal-dot-player/src/objects/Player.java
@@ -64,4 +64,10 @@ public class Player {
 	public Hitbox getHitbox() {
 		return this.hitbox;
 	}
+	public BufferedImage getSprite() {
+		return this.sprite;
+	}
+	public void setSprite(BufferedImage sprite) {
+		this.sprite = sprite;
+	}
 }
