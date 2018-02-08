27dc0127da677cfa830b8219be1f9dc85d3a9fe2
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-08 11:00:33 +0100

    Corrected loop, added jdoc and added repaint

--- yal-dot-player/src/core/Game.java
+++ yal-dot-player/src/core/Game.java
@@ -16,15 +16,24 @@ public class Game {
 			double startTime = System.nanoTime();
 			
 			//inner loop
-			this.update(System.nanoTime()-startTime);
 			this.handleInput(System.nanoTime()-startTime);
+			this.update(System.nanoTime()-startTime);
+			this.drawengine.repaint();
 		}
 	}
 	
+	/**
+	 * Updates objects and checks hitboxes
+	 * @param delta		the time it took for the last action
+	 */
 	public void update(double delta) {
 		System.out.printf("%f : Time Passed\n", (float)delta/100);
 	}
 	
+	/**
+	 * Handels input it got from Eventhandler
+	 * @param delta		the time it took for the last action
+	 */
 	public void handleInput(double delta) {
 		
 	}
