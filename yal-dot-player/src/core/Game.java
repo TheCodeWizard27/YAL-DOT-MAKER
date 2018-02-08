77914986a23c0ca01018587fb4167350efc0ef40
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-08 13:05:32 +0100

    Implemented input handler into game class

--- yal-dot-player/src/core/Game.java
+++ yal-dot-player/src/core/Game.java
@@ -7,9 +7,12 @@ public class Game {
 	private boolean run = true;
 	private Set<Integer> keyBuffer = new HashSet<Integer>();
 	private DrawEngine drawengine;
+	private EventHandler eventHandler;
 	
 	public Game() {
 		this.drawengine = DrawEngine.getInstance(this);
+		this.eventHandler = EventHandler.getInstance(this);
+		this.drawengine.addKeyListener(this.eventHandler);
 		
 		//main loop
 		while(this.run) {
@@ -27,7 +30,7 @@ public class Game {
 	 * @param delta		the time it took for the last action
 	 */
 	public void update(double delta) {
-		System.out.printf("%f : Time Passed\n", (float)delta/100);
+		//System.out.printf("%f : Time Passed\n", (float)delta/100);
 	}
 	
 	/**
@@ -35,6 +38,23 @@ public class Game {
 	 * @param delta		the time it took for the last action
 	 */
 	public void handleInput(double delta) {
+		try {
+		for(int key : this.keyBuffer) {
+			switch(key) {
+			default:
+				System.out.println(key);
+			}
+		}
+		}catch(java.util.ConcurrentModificationException e){
+			System.out.println("Error Error Error");
+		}
 		
+	}
+	
+	public void addInput(int key) {
+		this.keyBuffer.add(key);
+	}
+	public void removeInput(int key) {
+		this.keyBuffer.remove(key);
 	}
 }
