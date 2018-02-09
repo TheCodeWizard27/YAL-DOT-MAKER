13d5e699ea0af1ae5f738d826a182935f67d3e14
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-09 16:01:55 +0100

    Added repaint loop

--- yal-dot-maker/src/core/Controller.java
+++ yal-dot-maker/src/core/Controller.java
@@ -1,18 +1,34 @@
 package core;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class Controller implements MouseMotionListener, MouseListener, KeyListener{
+import javax.swing.Timer;
+
+public class Controller implements ActionListener, MouseMotionListener, MouseListener, KeyListener{
 	private Model model;
 	private View view;
+	private Timer timer;
 	
 	public Controller() {
 		this.model = new Model();
 		this.view = View.getInstance(this.model);
+		
+		this.timer = new Timer(500,this);
+		this.timer.start();
+	}
+	
+	/**
+	 * Automatically updates view if normal repainter's not added
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.view.repaint();
 	}
 	
 	@Override
