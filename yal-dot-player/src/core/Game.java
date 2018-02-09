81492c8dec53e6014bfbea7fbbb70c3876114239
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-09 08:56:03 +0100

    fixed fps timer and added simple movement

--- yal-dot-player/src/core/Game.java
+++ yal-dot-player/src/core/Game.java
@@ -1,19 +1,25 @@
 package core;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.HashSet;
 import java.util.Set;
+
+import javax.swing.Timer;
 
 import graphics.Vector2f;
 import objects.Player;
 import physiks.Hitbox;
 
-public class Game {
+public class Game implements ActionListener{
 	private boolean run = true;
+	private boolean hitboxVisible = true;
 	private Set<Integer> keyBuffer = new HashSet<Integer>();
 	private DrawEngine drawengine;
 	private EventHandler eventHandler;
 	private Map map;
+	private Timer fpsTimer = new Timer(40,this);
 	
 	public Game() {
 		this.setMap(new Map(null,null));
@@ -22,14 +28,18 @@ public class Game {
 		this.eventHandler = EventHandler.getInstance(this);
 		this.drawengine.addKeyListener(this.eventHandler);
 		
-		//main loop
-		while(this.run) {
-			double startTime = System.nanoTime();
-			
-			//inner loop
-			this.handleInput(System.nanoTime()-startTime);
-			this.update(System.nanoTime()-startTime);
+		this.fpsTimer.start();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(this.run) {
+			//game loop
+			this.handleInput();
+			this.update();
 			this.drawengine.repaint();
+		}else {
+			this.fpsTimer.stop();
 		}
 	}
 	
@@ -37,8 +47,10 @@ public class Game {
 	 * Updates objects and checks hitboxes
 	 * @param delta		the time it took for the last action
 	 */
-	public void update(double delta) {
+	public void update() {
 		Player player = this.map.getPlayer();
+		
+		//System.out.printf("%f : Milliseconds\n",delta);
 		
 		//hitbox detection
 		for(Hitbox hitbox : this.map.getHitboxes()) {
@@ -57,6 +69,7 @@ public class Game {
 				if(dirY < 0) {
 					float distance = (player.getHitbox().getPos2().getY() - hitbox.getPos().getY())-1;	//gets distance it should go back upwards
 					player.setYPos(player.getPos().getY() + distance);
+					player.setInAir(true);
 				}else if(dirY > 0) {
 					float distance = (player.getHitbox().getPos().getY() - hitbox.getPos2().getY())+1;	//gets distance it should go back downwards
 					player.setYPos(player.getPos().getY() + distance);
@@ -70,17 +83,22 @@ public class Game {
 	 * Handels input it got from Eventhandler
 	 * @param delta		the time it took for the last action
 	 */
-	public void handleInput(double delta) {
+	public void handleInput() {
+		Player player = this.map.getPlayer();
+		
 		try {
 		for(int key : this.keyBuffer) {
 			switch(key) {
 			case KeyEvent.VK_W: case KeyEvent.VK_UP:
+				player.setYPos(player.getPos().getX() - Player.JUMP_HEIGHT);
 				break;
 			case KeyEvent.VK_A: case KeyEvent.VK_LEFT:
+				player.setXPos(player.getPos().getX() - Player.WALK_SPEED);
 				break;
 			case KeyEvent.VK_S: case KeyEvent.VK_DOWN:
 				break;
 			case KeyEvent.VK_D: case KeyEvent.VK_RIGHT:
+				player.setXPos(player.getPos().getX() + Player.WALK_SPEED);
 				break;
 			default:
 				System.out.println(key);
@@ -104,5 +122,12 @@ public class Game {
 	}
 	public void setMap(Map map) {
 		this.map = map;
+	}
+
+	public boolean isHitboxVisible() {
+		return hitboxVisible;
+	}
+	public void setHitboxVisible(boolean hitboxVisible) {
+		this.hitboxVisible = hitboxVisible;
 	}
 }
