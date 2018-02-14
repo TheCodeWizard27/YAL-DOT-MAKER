4f35754c83a62be05f4f556da2f34fdd123623b5
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-14 15:06:57 +0100

    added Map properties tab

--- yal-dot-maker/src/GUI/Tabs.java
+++ yal-dot-maker/src/GUI/Tabs.java
@@ -2,28 +2,65 @@ package GUI;
 
 import java.awt.Dimension;
 
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
-import constants.Tab;
+import constants.MapProperty;
+import core.Model;
 
 public class Tabs {
 	private JTabbedPane tabs;
-	private JPanel[] panels = new JPanel[3];
+	private JPanel mapSettingsTab;
+	private JPanel objectExplorerTab;
+	private JPanel objectSettingsTab;
 	
-	public Tabs() {
+	public Tabs(Model model) {
 		this.tabs = new JTabbedPane();
 		this.tabs.setPreferredSize(new Dimension(350,400));
 		
-		int i = 0;
-		for(Tab tab : Tab.values()) {
-			this.panels[i] = new JPanel();
-			this.tabs.add(tab.toString().toLowerCase(), this.panels[i]);
-			i++;
-		}
+		this.addMapSettings(model);
+		this.addObjectExplorer();
+		this.addObjectSettings();
+		
+		this.tabs.add("Map Settings", this.mapSettingsTab);
+		this.tabs.add("Object Explorer", this.objectExplorerTab);
+		this.tabs.add("Object Settings", this.objectSettingsTab);
 	}
 	
 	public JTabbedPane getTabBar() {
 		return this.tabs;
+	}
+	
+	public void addMapSettings(Model model) {
+		this.mapSettingsTab = new JPanel();
+		
+		for(MapProperty property : MapProperty.values()) {
+			JPanel tempContainer = new JPanel();
+			JLabel label = new JLabel();
+			switch(property) {
+			case NAME:
+				label.setText("Map Name : ");
+				break;
+			case WIDTH:
+				label.setText("Map width : ");
+				break;
+			case HEIGHT:
+				label.setText("Map height : ");
+				break;
+			}
+			tempContainer.add(label);
+			tempContainer.add(new InputField(model.getMap(),property));
+			this.mapSettingsTab.add(tempContainer);
+		}
+	}
+	
+	public void addObjectExplorer() {
+		this.objectExplorerTab = new JPanel();
+	}
+	
+	public void addObjectSettings() {
+		this.objectSettingsTab = new JPanel();
 	}
 }
