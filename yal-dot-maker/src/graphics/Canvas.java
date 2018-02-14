5961bb60c3ebd057aeef314de4aa7c85690b1299
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-14 09:32:52 +0100

    Added rezisable canvas screen for displaying the map and added backdrop

--- yal-dot-maker/src/graphics/Canvas.java
+++ yal-dot-maker/src/graphics/Canvas.java
@@ -1,21 +1,47 @@
 package graphics;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.GridBagLayout;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
-import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import core.Model;
 
 public class Canvas extends JPanel{
 	private Model model;
+	private JScrollPane scrollBar;
 	
 	public Canvas(Model model) {
 		super();
+		this.model = model;
+		
+		this.scrollBar = new JScrollPane(this);
+		this.scrollBar.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		this.scrollBar.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		
 		this.setBackground(Color.BLACK);
+		this.setPreferredSize(new Dimension((int)this.model.getMap().getSize().getX(),(int)this.model.getMap().getSize().getY()));
+	}
+	
+	public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
+		this.setPreferredSize(new Dimension((int)this.model.getMap().getSize().getX(),(int)this.model.getMap().getSize().getY()));
+		
+		BufferedImage testImg = new BufferedImage(1200,800,BufferedImage.TYPE_INT_ARGB);
+		Graphics2D tg2d = (Graphics2D) testImg.getGraphics();
+		
+		tg2d.fillRect(0, 0, (int)this.model.getMap().getSize().getX(), (int)this.model.getMap().getSize().getY());
+		
+		g2d.drawImage(testImg, 0, 0, null);
+		
+	}
+	
+	public JScrollPane getCanvas() {
+		return this.scrollBar;
 	}
 }
