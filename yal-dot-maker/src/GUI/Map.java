d94be9dfb2d02ff1b8ecea9babc189c4529f843a
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-14 09:33:54 +0100

    added getters n' setters for map

--- yal-dot-maker/src/GUI/Map.java
+++ yal-dot-maker/src/GUI/Map.java
@@ -1,6 +1,5 @@
 package GUI;
 
-import java.awt.List;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
@@ -23,6 +22,61 @@ public class Map {
 	private BufferedImage backgroundImage;
 	
 	public Map() {
-		
+		this.size = new Vector2f(800,600);
+	}
+	
+	public BufferedImage getPlayer() {
+		return player;
+	}
+	public void setPlayer(BufferedImage player) {
+		this.player = player;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Vector2f getSize() {
+		return size;
+	}
+	public void setSize(Vector2f size) {
+		this.size = size;
+	}
+	public EndBox getEndBox() {
+		return endBox;
+	}
+	public void setEndBox(EndBox endBox) {
+		this.endBox = endBox;
+	}
+	public Camera getCamera() {
+		return camera;
+	}
+	public void setCamera(Camera camera) {
+		this.camera = camera;
+	}
+	public ArrayList<Asset> getAssets() {
+		return assets;
+	}
+	public void setAssets(ArrayList<Asset> assets) {
+		this.assets = assets;
+	}
+	public ArrayList<Hitbox> getHitboxes() {
+		return hitboxes;
+	}
+	public void setHitboxes(ArrayList<Hitbox> hitboxes) {
+		this.hitboxes = hitboxes;
+	}
+	public ArrayList<DeathBoxes> getDeathboxes() {
+		return deathboxes;
+	}
+	public void setDeathboxes(ArrayList<DeathBoxes> deathboxes) {
+		this.deathboxes = deathboxes;
+	}
+	public BufferedImage getBackgroundImage() {
+		return backgroundImage;
+	}
+	public void setBackgroundImage(BufferedImage backgroundImage) {
+		this.backgroundImage = backgroundImage;
 	}
 }
