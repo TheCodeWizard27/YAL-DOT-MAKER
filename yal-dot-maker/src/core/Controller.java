7de9c6e7c17c9e9f7c71d35452f72867e588edfe
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-14 12:33:56 +0100

    added zoom control

--- yal-dot-maker/src/core/Controller.java
+++ yal-dot-maker/src/core/Controller.java
@@ -7,10 +7,12 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
 import javax.swing.Timer;
 
-public class Controller implements ActionListener, MouseMotionListener, MouseListener, KeyListener{
+public class Controller implements ActionListener, MouseMotionListener, MouseListener, KeyListener, MouseWheelListener{
 	private Model model;
 	private View view;
 	private Timer timer;
@@ -18,6 +20,9 @@ public class Controller implements ActionListener, MouseMotionListener, MouseLis
 	public Controller() {
 		this.model = new Model();
 		this.view = View.getInstance(this.model);
+		
+		this.view.getCanvas().addKeyListener(this);
+		this.view.getCanvas().addMouseWheelListener(this);
 		
 		this.timer = new Timer(20,this);
 		this.timer.start();
@@ -28,18 +33,25 @@ public class Controller implements ActionListener, MouseMotionListener, MouseLis
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		this.view.getCanvas().requestFocusInWindow();
 		this.view.repaint();
 	}
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_CONTROL:
+			this.model.setCtrl(true);
+			break;
+		}
 	}
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_CONTROL:
+			this.model.setCtrl(false);
+			break;
+		}
 	}
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -80,5 +92,17 @@ public class Controller implements ActionListener, MouseMotionListener, MouseLis
 	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent e) {
+		if(this.model.isCtrl()) {
+			if(e.getWheelRotation() < 0 && this.model.getZoom() < 500) {
+				this.model.setZoom(this.model.getZoom() + 10f);
+			}else if(e.getWheelRotation() > 0 && this.model.getZoom() > 20){
+				this.model.setZoom(this.model.getZoom() - 10f);
+			}
+			this.view.getInfoBar().getZoom().setText("Zoom: " + Integer.toString((int)this.model.getZoom())+"%");
+			this.view.getCanvas().getCanvas().setViewportView(this.view.getCanvas());
+		}
 	}
 }
