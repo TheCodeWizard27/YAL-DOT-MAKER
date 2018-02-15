783669ad0161951c6961b0aa4ba0f809a09edb8c
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-15 17:34:14 +0100

    added copy constructor

--- yal-dot-maker/src/map/Deathbox.java
+++ yal-dot-maker/src/map/Deathbox.java
@@ -7,5 +7,9 @@ public class Deathbox extends Hitbox{
 	public Deathbox(String name, Vector2f pos, Vector2f size) {
 		super(name, pos, size);
 	}
+	
+	public Deathbox(Deathbox deathbox) {
+		super(deathbox.getName(), new Vector2f(deathbox.getPos()),new Vector2f(deathbox.getSize()));
+	}
 
 }
