6d4c231fca3b89639d5c1430a0b16435645049e1
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-16 08:52:39 +0100

    changed hitbox functions

--- yal-dot-maker/src/map/Hitbox.java
+++ yal-dot-maker/src/map/Hitbox.java
@@ -2,33 +2,10 @@ package map;
 
 import graphics.Vector2f;
 
-public class Hitbox {
-	private String name;
-	private Vector2f pos;
-	private Vector2f size;
-	
-	public Hitbox(String name, Vector2f pos, Vector2f size) {
-		this.name = name;
-		this.pos = pos;
-		this.size = size;
+public class Hitbox extends ElementTemplate{
+
+	protected Hitbox(String name, Vector2f pos, Vector2f size) {
+		super(name, pos, size);
 	}
 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Vector2f getPos() {
-		return pos;
-	}
-	public void setPos(Vector2f pos) {
-		this.pos = pos;
-	}
-	public Vector2f getSize() {
-		return size;
-	}
-	public void setSize(Vector2f size) {
-		this.size = size;
-	}
 }
