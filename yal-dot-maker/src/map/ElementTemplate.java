c26e50007f44ba795eebc420780ef77569fb1cc9
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-16 08:50:31 +0100

    added super getters n' setters for super class

--- yal-dot-maker/src/map/ElementTemplate.java
+++ yal-dot-maker/src/map/ElementTemplate.java
@@ -3,13 +3,32 @@ package map;
 import graphics.Vector2f;
 
 public class ElementTemplate {
-	private String name;
-	private Vector2f pos;
-	private Vector2f size;
+	protected String name;
+	protected Vector2f pos;
+	protected Vector2f size;
 	
-	private ElementTemplate(String name, Vector2f pos, Vector2f size) {
+	protected ElementTemplate(String name, Vector2f pos, Vector2f size) {
 		this.name = name;
 		this.pos = new Vector2f(pos);
 		this.size = new Vector2f(size);
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Vector2f getPos() {
+		return pos;
+	}
+	public void setPos(Vector2f pos) {
+		this.pos = pos;
+	}
+	public Vector2f getSize() {
+		return size;
+	}
+	public void setSize(Vector2f size) {
+		this.size = size;
 	}
 }
