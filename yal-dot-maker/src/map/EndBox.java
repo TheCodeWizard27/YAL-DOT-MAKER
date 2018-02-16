b1725c031e07ae518179c4aeb233467bcde3ac8d
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-16 08:53:18 +0100

    changed extension from hitbox to elementtemplate

--- yal-dot-maker/src/map/EndBox.java
+++ yal-dot-maker/src/map/EndBox.java
@@ -2,7 +2,7 @@ package map;
 
 import graphics.Vector2f;
 
-public class EndBox extends Hitbox{
+public class EndBox extends ElementTemplate{
 
 	public EndBox(String name, Vector2f pos, Vector2f size) {
 		super(name, pos, size);
