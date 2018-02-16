de94aac610bc8d78e7fb60df36e16e3aa73f579f
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-16 08:51:39 +0100

    extended camera class

--- yal-dot-maker/src/map/Camera.java
+++ yal-dot-maker/src/map/Camera.java
@@ -1,5 +1,11 @@
 package map;
 
-public class Camera {
+import graphics.Vector2f;
+
+public class Camera extends ElementTemplate{
+
+	protected Camera(Vector2f pos, Vector2f size) {
+		super("Camera", pos, size);
+	}
 
 }
