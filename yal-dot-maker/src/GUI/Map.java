9c61df44b0835e76ce58cc5f3ea37e8200517e40
TheCodeWizard27 <benny.schaerer@gmail.com>
2018-02-21 13:42:59 +0100

    Removed useless stuff

--- yal-dot-maker/src/GUI/Map.java
+++ yal-dot-maker/src/GUI/Map.java
@@ -12,73 +12,61 @@ import map.Hitbox;
 import map.Player;
 
 public class Map {
-	private Player player = new Player(new Vector2f(0,0),new Vector2f(38,64));
+	private Player player = new Player(new Vector2f(0, 0), new Vector2f(38, 64));
 	private String name;
 	private Vector2f size;
 	private ArrayList<EndBox> endBox = new ArrayList<EndBox>();
-	private Camera camera = new Camera(new Vector2f(0,0),new Vector2f(450,300));
+	private Camera camera = new Camera(new Vector2f(0, 0), new Vector2f(450, 300));
 	private ArrayList<Asset> assets = new ArrayList<Asset>();
 	private ArrayList<Hitbox> hitboxes = new ArrayList<Hitbox>();
 	private ArrayList<Deathbox> deathboxes = new ArrayList<Deathbox>();
-	private BufferedImage backgroundImage = new BufferedImage(450,300,BufferedImage.TYPE_INT_ARGB);
-	
+	private BufferedImage backgroundImage = new BufferedImage(450, 300, BufferedImage.TYPE_INT_ARGB);
+
 	public Map() {
 		this.name = "Costum Map";
-		this.size = new Vector2f(500,500);
+		this.size = new Vector2f(500, 500);
 	}
-	
+
 	public Player getPlayer() {
 		return player;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Vector2f getSize() {
 		return size;
 	}
+
 	public ArrayList<EndBox> getEndBox() {
 		return endBox;
 	}
+
 	public Camera getCamera() {
 		return camera;
 	}
-	
+
 	public ArrayList<Asset> getAssets() {
 		return assets;
 	}
-	public void addAsset(Asset asset) {
-		this.assets.add(asset);
-	}
-	public void rmAsset(Asset asset) {
-		this.assets.remove(asset);
-	}
-	
+
 	public ArrayList<Hitbox> getHitboxes() {
 		return hitboxes;
 	}
-	public void addHitbox(Hitbox hitbox) {
-		this.hitboxes.add(hitbox);
-	}
-	public void rmHitbox(Hitbox hitbox) {
-		this.hitboxes.remove(hitbox);
-	}
-	
+
 	public ArrayList<Deathbox> getDeathboxes() {
 		return deathboxes;
 	}
-	public void addDeathbox(Deathbox deathbox) {
-		this.deathboxes.add(deathbox);
-	}
-	public void rmDeathbox(Deathbox deathbox) {
-		this.deathboxes.remove(deathbox);
-	}
-	
+
 	public BufferedImage getBackgroundImage() {
 		return backgroundImage;
 	}
+
 	public void setBackgroundImage(BufferedImage backgroundImage) {
 		this.backgroundImage = backgroundImage;
 	}
--- yal-dot-maker/src/core/View.java
+++ yal-dot-maker/src/core/View.java
@@ -13,7 +13,7 @@ import GUI.ObjectList;
 import GUI.Tabs;
 import graphics.Canvas;
 
-public class View extends JFrame{
+public class View extends JFrame {
 	private static View view;
 	private Model model;
 	private MenuBar menu;
@@ -23,96 +23,109 @@ public class View extends JFrame{
 	private InfoBar infoBar;
 	private Canvas canvas;
 	private JPanel container;
-	
+
 	private View(Model model) {
 		super("YAL DOT MAKER");
-		this.tabs = new Tabs(model,this);
+		this.tabs = new Tabs(model, this);
 		this.objectList = new ObjectList(model, this);
-		this.west = new JSplitPane(JSplitPane.VERTICAL_SPLIT,this.tabs.getTabBar(),this.objectList.getObjectList());
+		this.west = new JSplitPane(JSplitPane.VERTICAL_SPLIT, this.tabs.getTabBar(), this.objectList.getObjectList());
 		this.infoBar = new InfoBar();
 		this.canvas = new Canvas(model, this);
-		this.menu = new MenuBar(model,this);
+		this.menu = new MenuBar(model, this);
 		this.model = model;
 		this.container = new JPanel();
-		
-		this.setSize(1200,800);
+
+		this.setSize(1200, 800);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setJMenuBar(this.menu.getMenuBar());
-		
+
 		this.container.setLayout(new BorderLayout());
 		this.container.add(this.infoBar.getInfoBar(), BorderLayout.SOUTH);
-		this.container.add(new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,this.west,this.canvas.getCanvas()), BorderLayout.CENTER);
-		
+		this.container.add(new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, this.west, this.canvas.getCanvas()),
+				BorderLayout.CENTER);
+
 		this.west.setDividerLocation(520);
 		this.add(this.container);
 		this.setVisible(true);
 	}
-	
+
 	public void resetGUI() {
-		
+
 		this.getContentPane().removeAll();
-		this.tabs = new Tabs(this.model,this);
+		this.tabs = new Tabs(this.model, this);
 		this.objectList = new ObjectList(model, this);
-		this.west = new JSplitPane(JSplitPane.VERTICAL_SPLIT,this.tabs.getTabBar(),this.objectList.getObjectList());
+		this.west = new JSplitPane(JSplitPane.VERTICAL_SPLIT, this.tabs.getTabBar(), this.objectList.getObjectList());
 		this.infoBar = new InfoBar();
-		this.menu = new MenuBar(this.model,this);
+		this.menu = new MenuBar(this.model, this);
 		this.container = new JPanel();
-		
+
 		this.setJMenuBar(this.menu.getMenuBar());
-		
+
 		this.container.setLayout(new BorderLayout());
 		this.container.add(this.infoBar.getInfoBar(), BorderLayout.SOUTH);
-		this.container.add(new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,this.west,this.canvas.getCanvas()), BorderLayout.CENTER);
-		
+		this.container.add(new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, this.west, this.canvas.getCanvas()),
+				BorderLayout.CENTER);
+
 		this.west.setDividerLocation(520);
 		this.add(this.container);
-		
+
 		this.objectList.update();
 		this.revalidate();
 		this.setVisible(true);
 	}
-	
+
 	public static View getInstance(Model model) {
-		if(View.view == null) {
+		if (View.view == null) {
 			View.view = new View(model);
 		}
-		
+
 		return View.view;
 	}
-	
+
 	public ObjectList getObjectList() {
 		return objectList;
 	}
+
 	public void setObjectList(ObjectList objectList) {
 		this.objectList = objectList;
 	}
+
 	public Canvas getCanvas() {
 		return this.canvas;
 	}
+
 	public Model getModel() {
 		return model;
 	}
+
 	public void setModel(Model model) {
 		this.model = model;
 	}
+
 	public MenuBar getMenu() {
 		return menu;
 	}
+
 	public void setMenu(MenuBar menu) {
 		this.menu = menu;
 	}
+
 	public InfoBar getInfoBar() {
 		return infoBar;
 	}
+
 	public void setInfoBar(InfoBar infoBar) {
 		this.infoBar = infoBar;
 	}
+
 	public JPanel getContainer() {
 		return this.container;
 	}
+
 	public Tabs getTabs() {
 		return tabs;
 	}
+
 	public void setTabs(Tabs tabs) {
 		this.tabs = tabs;
 	}
